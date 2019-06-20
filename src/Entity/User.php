f64c69ca4bb6b14e2938494973337f5f35ea4380
bilalbrgt <47894927+bilalbrgt@users.noreply.github.com>
2019-06-20 11:08:11 +0200

    first commit

---
+++ src/Entity/User.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Passwords;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->Email;
+    }
+
+    public function setEmail(string $Email): self
+    {
+        $this->Email = $Email;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->Username;
+    }
+
+    public function setUsername(string $Username): self
+    {
+        $this->Username = $Username;
+
+        return $this;
+    }
+
+    public function getPasswords(): ?string
+    {
+        return $this->Passwords;
+    }
+
+    public function setPasswords(string $Passwords): self
+    {
+        $this->Passwords = $Passwords;
+
+        return $this;
+    }
+}
